eb41d2c68b12ce45fdacfb68b95cce233fbd1606
Loc Mai <locmai0201@gmail.com>
2021-12-20 04:14:31 +0700

    fix: lint

--- tests/test_path.py
+++ tests/test_path.py
@@ -1,15 +1,16 @@
-import pytest
+from ..main import app
 from fastapi.testclient import TestClient
+
 import sys
 import os.path
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
-from ..main import app
 
 client = TestClient(app)
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 
 def test_health_path():
     response = client.get("/")
